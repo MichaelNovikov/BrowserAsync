2ae7b76ad075dbbc0aff798713061b8d2b18b7f2
Novikov <novikovmailg@gmail.com>
2018-10-30 18:41:10 +0200

    Add project files.

---
+++ BrowserAsync/BrowserAsync/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BrowserAsync
+{
+    public partial class MainPage : ContentPage
+    {
+        HtmlSource htmlSource;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            htmlSource = new HtmlSource();
+            btnGo.Clicked += BtnGo_Click;
+        }
+
+        private async void BtnGo_Click(object sender, EventArgs e)
+        {
+            Spinner.IsRunning = true;
+
+            var urlTxt = txtEntry.Text;
+            var result = await htmlSource.GetHtmlAsync(urlTxt);
+
+            Browser.Source = result;
+            Spinner.IsRunning = false;
+        } 
+    }
+}
---
+++ BrowserAsync/BrowserAsync/Model/HtmlSource.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BrowserAsync
+{
+    public class HtmlSource
+    {
+        public HtmlWebViewSource GetHtml(string urlStr)
+        {
+            var htmlSource = new HtmlWebViewSource();
+            string html = "";
+            string url = @"https://";
+            url += urlStr;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.AutomaticDecompression = DecompressionMethods.GZip;
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    html = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                htmlSource.Html = ex.Message;
+                return htmlSource;
+            }
+
+            htmlSource.Html = html;
+            return htmlSource;
+        }
+
+        public async Task<HtmlWebViewSource> GetHtmlAsync(string urlStr)
+        {
+           return await Task.Run(() => GetHtml(urlStr));
+        }
+    }
+}
